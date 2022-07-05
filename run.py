e2d2f2035f6f8986fd91fafaa1d62dea70749182
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-07-05 19:24:46 +0000

    Add template inheritance functionality to reduce code in all pages

--- run.py
+++ run.py
@@ -23,6 +23,12 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/careers")
+def careers():
+    """Define route for careers page"""
+    return render_template("careers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
