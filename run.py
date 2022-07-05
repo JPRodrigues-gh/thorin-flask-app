f0460992f6d87ad1807725bfadb98d794191d969
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-07-05 19:01:35 +0000

    Add flask routing to render html pages

---
+++ run.py
@@ -0,0 +1,31 @@
+"""Thorin flask app python file"""
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Define route for index page"""
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """Define route for about page"""
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    """Define route for contact page"""
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
